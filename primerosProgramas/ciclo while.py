8c5989bac5ca739bbceabcecfcbe2d22bd79e115
jsebastianrodriguez <142937823+jsebastianrodriguez@users.noreply.github.com>
2023-09-18 16:32:01 -0500

    Add files via upload

---
+++ primerosProgramas/ciclo while.py
@@ -0,0 +1,13 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6") 
+
+
+for i in range(1,6):
+    while i <= 4:
+        i += 1
+        print(i)
+    break    
